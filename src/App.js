3e520e43503125cb76ce472ea6d3c881cc197010
Anna Malysheva <anna_malysheva@epam.com>
2024-01-25 19:48:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <div className="awesomeButton" onClick={handleClick}>
+          <span>Care to click?</span>
+          {isOpen && (
+              <div className="textMessage">
+                Look at yooouuu!
+              </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
